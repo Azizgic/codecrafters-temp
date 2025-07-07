f96dad06a29e036e1c23b9d94cfaa364996ca574
Azizgic <azizgichki3@gmail.com>
2025-07-07 13:59:42 +0500

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log("Recieved request: \n", data.toString());
+
+    const CRLF = "\r\n";
+    let response = "HTTP/1.1 200 OK" + CRLF + CRLF;
+    socket.write(response);
+  });
+
   socket.on("close", () => {
     socket.end();
   });
 });
 
 server.listen(4221, () => {
-  console.log("opened server on : ", server.address());
+  console.log("opened server on : ", "localhost", server.address());
 });
